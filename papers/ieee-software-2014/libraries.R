7a7564710be650dde3a431557d479572a3c37cd3
Stefan Marr <git@stefan-marr.de>
2014-05-29 18:04:00 +0100

    Added plots from Are We Fast Yet paper in IEEE Software

---
+++ papers/ieee-software-2014/libraries.R
@@ -0,0 +1,10 @@
+writeLines("[INFO] Loading Libraries")
+
+library(plyr)
+library(ggplot2)
+library(psych)   # uses only geometric.mean
+library(tables)
+
+source("data-processing.R")
+source("plots.R")
+source("colors.R")
